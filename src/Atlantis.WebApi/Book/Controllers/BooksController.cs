3ccde3f83c36b7b3946d902339322f1629d2ce71
Vedant Phougat <vedant.phougat@hotmail.com>
2021-09-13 10:37:41 +0530

    Added books controller

---
+++ src/Atlantis.WebApi/Book/Controllers/BooksController.cs
@@ -0,0 +1,15 @@
+﻿namespace Atlantis.WebApi.Book.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("Ping!!!");
+        }
+    }
+}
