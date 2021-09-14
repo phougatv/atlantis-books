f96d21f41f1fc8dac5c1de39946337361ca6c0e2
Vedant Phougat <vedant.phougat@hotmail.com>
2021-09-15 00:28:07 +0530

    Added book extension.

---
+++ src/Atlantis.WebApi/Book/Extensions/Startup.BookExtension.cs
@@ -0,0 +1,24 @@
+﻿namespace Atlantis.WebApi.Book.Extensions
+{
+    using Atlantis.WebApi.Book.Business;
+    using Atlantis.WebApi.Book.Persistence;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    internal static class BookExtension
+    {
+        internal static IServiceCollection AddAtlantisBook(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddAtlantisDbContext(configuration)
+                .AddScoped<IBookService, BookService>()
+                .AddScoped<IBookRepository, BookRepository>();
+
+            return services;
+        }
+
+        internal static IServiceCollection AddAtlantisDbContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<AtlantisDbContext>(optionsBuilder => optionsBuilder.UseSqlServer(configuration.GetSection("ConnectionString").Value));
+    }
+}
