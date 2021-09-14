31addf25756a741bfadc6fad5ca052d15ab381c7
Vedant Phougat <vedant.phougat@hotmail.com>
2021-09-14 23:01:38 +0530

    Added CRUD operations.

--- src/Atlantis.WebApi/Shared/DataAccess/Base/Repository/EfRepository.cs
+++ src/Atlantis.WebApi/Shared/DataAccess/Base/Repository/EfRepository.cs
@@ -1,6 +1,8 @@
 ﻿namespace Atlantis.WebApi.Shared.DataAccess.Base.Repository
 {
     using Atlantis.WebApi.Book.Persistence;
+    using Microsoft.EntityFrameworkCore;
+    using System;
 
     internal abstract class EfRepository<TEntity, TId> : IRepository<TEntity, TId>
         where TEntity : Entity<TId>
@@ -17,12 +19,60 @@
             _dbContext = dbContext;
         }
 
+        #region Public CRUD Methods
+        /// <summary>
+        /// Creates an <see cref="TEntity"/>.
+        /// </summary>
+        /// <param name="entity">The entity of type <typeparamref name="TEntity"/>.</param>
+        /// <returns></returns>
+        bool IRepository<TEntity, TId>.Create(TEntity entity)
+        {
+            entity.CreatedOn = DateTime.UtcNow;
+            var entityEntry = _dbContext.Set<TEntity>().Add(entity);
+
+            return entityEntry.State == EntityState.Added;
+        }
+
         /// <summary>
         /// Reads an <see cref="TEntity"/> based on id.
         /// </summary>
         /// <param name="id">The <typeparamref name="TId"/>.</param>
         /// <returns></returns>
         TEntity IRepository<TEntity, TId>.Read(TId id) => InternalReadById(id);
+
+        /// <summary>
+        /// Updates an <see cref="TEntity"/>.
+        /// </summary>
+        /// <param name="entity">The entity of type <typeparamref name="TEntity"/>.</param>
+        /// <returns></returns>
+        bool IRepository<TEntity, TId>.Update(TEntity entity)
+        {
+            var internalEntity = InternalReadById(entity.Id);
+            entity.CreatedOn = internalEntity.CreatedOn;
+            entity.UpdatedOn = DateTime.UtcNow;
+            _dbContext.Entry(internalEntity).State = EntityState.Detached;
+
+            var entityEntry = _dbContext.Attach(entity);
+            entityEntry.State = EntityState.Modified;
+
+            return entityEntry.State == EntityState.Modified;
+        }
+
+        /// <summary>
+        /// Deletes an <see cref="TEntity"/> based on id.
+        /// </summary>
+        /// <param name="id">The id of type <typeparamref name="TId"/>.</param>
+        /// <returns></returns>
+        bool IRepository<TEntity, TId>.Delete(TId id)
+        {
+            var entity = InternalReadById(id);
+            if (entity == null)
+                return false;
+
+            var entityEntry = _dbContext.Set<TEntity>().Remove(entity);
+            return entityEntry.State == EntityState.Deleted;
+        }
+        #endregion Public CRUD Methods
 
         #region Protected Abstract Methods
         protected virtual TEntity InternalReadById(TId id) => _dbContext.Set<TEntity>().Find(id);
--- src/Atlantis.WebApi/Shared/DataAccess/Base/Repository/IRepository.cs
+++ src/Atlantis.WebApi/Shared/DataAccess/Base/Repository/IRepository.cs
@@ -5,10 +5,31 @@
         where TId : struct
     {
         /// <summary>
+        /// Creates an <see cref="TEntity"/>.
+        /// </summary>
+        /// <param name="entity">The entity of type <typeparamref name="TEntity"/>.</param>
+        /// <returns></returns>
+        bool Create(TEntity entity);
+
+        /// <summary>
         /// Reads an <see cref="TEntity"/> based on id.
         /// </summary>
-        /// <param name="id">The <typeparamref name="TId"/>.</param>
+        /// <param name="id">The id of type <typeparamref name="TId"/>.</param>
         /// <returns></returns>
         TEntity Read(TId id);
+
+        /// <summary>
+        /// Updates an <see cref="TEntity"/>.
+        /// </summary>
+        /// <param name="entity">The entity of type <typeparamref name="TEntity"/>.</param>
+        /// <returns></returns>
+        bool Update(TEntity entity);
+
+        /// <summary>
+        /// Deletes an <see cref="TEntity"/> based on id.
+        /// </summary>
+        /// <param name="id">The id of type <typeparamref name="TId"/>.</param>
+        /// <returns></returns>
+        bool Delete(TId id);
     }
 }
