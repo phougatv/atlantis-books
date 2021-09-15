536cc5d536e8d5aff8c6f0f40efc82ef28c7bc09
Vedant Phougat <vedant.phougat@hotmail.com>
2021-09-15 10:26:25 +0530

    Added unit of work

---
+++ src/Atlantis.WebApi/Shared/DataAccess/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿namespace Atlantis.WebApi.Shared.DataAccess.UnitOfWork
+{
+    /// <summary>
+    /// IUnitOfWork interface.
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        /// <summary>
+        /// Commits all the entity entry changes.
+        /// </summary>
+        /// <returns></returns>
+        int Commit();
+    }
+}
---
+++ src/Atlantis.WebApi/Shared/DataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿namespace Atlantis.WebApi.Shared.DataAccess.UnitOfWork
+{
+    using Atlantis.WebApi.Book.Persistence;
+
+    public class UnitOfWork
+    {
+        private readonly AtlantisDbContext _dbContext;
+
+        public UnitOfWork(AtlantisDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// Commits all the DB changes.
+        /// </summary>
+        /// <returns></returns>
+        public int Commit() => _dbContext.SaveChanges();
+    }
+}
