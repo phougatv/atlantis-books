0754d3ebb28b2ff2888643dcf1ed43deaebb704a
Vedant Phougat <vedant.phougat@hotmail.com>
2021-09-15 10:22:02 +0530

    Added order profile for automapper

---
+++ src/Atlantis.WebApi/Order/Profiles/OrderProfile.cs
@@ -0,0 +1,18 @@
+﻿namespace Atlantis.WebApi.Order.Profiles
+{
+    using Atlantis.WebApi.Order.Business;
+    using Atlantis.WebApi.Order.Dtos;
+    using Atlantis.WebApi.Order.Persistence;
+    using AutoMapper;
+
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<CartAddDto, CartDomainModel>();
+            CreateMap<CartUpdateDto, CartDomainModel>();
+            CreateMap<OrderDto, OrderDomainModel>();
+            CreateMap<OrderDomainModel, Order>();
+        }
+    }
+}
