df0e2d13a4df06a5716ba0e521e081ee53383600
Vedant Phougat <vedant.phougat@hotmail.com>
2021-09-18 02:43:23 +0530

    Added common extension class.

---
+++ src/Atlantis.WebApi/Shared/Extensions/CommonExtension.cs
@@ -0,0 +1,21 @@
+﻿namespace Atlantis.WebApi.Shared.Extensions
+{
+    using System;
+
+    public static class CommonExtension
+    {
+        #region Guid Extension Methods
+        public static bool IsEmpty(this Guid guid) => guid == Guid.Empty;
+        #endregion Guid Extension Methods
+
+        #region Int Extension Methods
+        public static bool IsNegative(this int int32) => int32 < 0;
+        public static bool IsPositive(this int int32) => int32 > 0;
+        public static bool IsZero(this int int32) => int32 == 0;
+        #endregion Int Extension Methods
+
+        #region String Extension Methods
+        public static bool IsNullOrEmpty(this string str) => string.IsNullOrEmpty(str);
+        #endregion String Extension Methods
+    }
+}
